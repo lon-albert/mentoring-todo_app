51b56830bfd582d034775c5f02e7842a3dcd6453
lonAlbert <lonilesanmi@gmail.com>
2017-02-22 11:08:21 +0100

    MainActivity and CreateToDoActivity

--- app/src/main/java/com/android/ilab/todoapp/CreateTodoActivity.java
+++ app/src/main/java/com/android/ilab/todoapp/CreateTodoActivity.java
@@ -1,13 +1,18 @@
 package com.android.ilab.todoapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
 public class CreateTodoActivity extends AppCompatActivity {
+
+    AutoCompleteTextView titleView;
+    EditText detailView;
+    AppCompatButton sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +21,46 @@ public class CreateTodoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        titleView = (AutoCompleteTextView) findViewById(R.id.title);
+        detailView = (EditText) findViewById(R.id.detail);
+        sendButton = (AppCompatButton) findViewById(R.id.create_button);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        sendButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        postData();
+                    }
+                }
+        );
+
     }
+
+    private void postData() {
+        if(!validateData()) {
+            return;
+        }
+
+        // TODO Implement posting to server
+    }
+
+    private boolean validateData() {
+        String title = titleView.getText().toString();
+        String detail = detailView.getText().toString();
+
+        if(title.length() < 3 || title.length() > 20){
+            titleView.setError("Must be between 3 and 20 characters");
+        }
+
+        if((detail.length() < 5 || detail.length() > 200)){
+            detailView.setError("Must be between 5 and 200 characters");
+        }
+
+        return false;
+
+    }
+
 
 }
